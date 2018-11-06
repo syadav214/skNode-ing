bc9924720ef581b61111d2d90c71b725b111527c
Santoshkumar Yadav <syadav214@gmail.com>
2018-11-06 16:10:07 +0530

    FS-2056 Alignment and font color

--- HtmlToPdf/index.js
+++ HtmlToPdf/index.js
@@ -1,7 +1,7 @@
 var fs = require('fs');
 var pdf = require('html-pdf');
 var html = fs.readFileSync('./index.html', 'utf8');
-var options = { format: 'Letter' };
+var options = { format: 'A4' };
 
 pdf.create(html, options).toFile('./index.pdf', function(err, res) {
   if (err) return console.log(err);
