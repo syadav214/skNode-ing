90bfb7e3bad8f057a6467b94f78247f0e1ce169e
Santoshkumar Yadav <syadav214@gmail.com>
2019-01-24 16:55:31 +0530

    default expression comments

--- SpareTime/defaultValueExpression.js
+++ SpareTime/defaultValueExpression.js
@@ -4,6 +4,6 @@ function a(i=1+y, j=2+z) {
   return i+j;
 }
 
-console.log(a());
+console.log(a()); // it use value of y & z and return 10 all the time
 
-console.log(a(2,2));
+console.log(a(2,2)); // it use value of y & Z and returns sum of arguments
